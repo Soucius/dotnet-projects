f7345274ed6b0bd5612ec7dc7ffe71718d7ca47d
Soucius <soucius.ggwp@gmail.com>
2025-04-11 16:58:05 +0200

    Views edited

--- dotnet-basics/Controllers/ProductsController.cs
+++ dotnet-basics/Controllers/ProductsController.cs
@@ -3,15 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 namespace dotnet_basics.Controllers;
 
 public class ProductsController : Controller {
-    public string Index() {
-        return "Products/Index";
+    public ActionResult Index() {
+        return View();
     }
 
-    public string List() {
-        return "Products/List";
+    public ActionResult List() {
+        return View();
     }
 
-    public string Details() {
-        return "Products/Details";
+    public ActionResult Details() {
+        return View();
     }
 }
