9be1c0f74e885ea125a024f0e34fc96a8d3796e6
Soucius <soucius.ggwp@gmail.com>
2025-04-12 17:44:47 +0200

    Static files

--- dotnet-basics/obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
+++ dotnet-basics/obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cfe44d606d5176d5c8bdc5c2a8eb5ef90544326")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe81726e268d4ce4d866ded277940c96defe4fab")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
