8e398db91f53c936bafd7b18af1d021471245634
Soucius <soucius.ggwp@gmail.com>
2025-04-14 11:49:13 +0200

    ViewData variables

--- dotnet-basics/Controllers/ProductsController.cs
+++ dotnet-basics/Controllers/ProductsController.cs
@@ -12,6 +12,18 @@ public class ProductsController : Controller {
     }
 
     public ActionResult Details() {
+        string urunBaslik = "Samsung S24 Ultra";
+        string urunAciklama = "Samsung Galaxy S24 Ultra 512GB 12GB Ram (Samsung Turkiye Garantili) Siyah";
+        double urunFiyat = 70000;
+        string urunResim = "samsung-s24.jpg";
+        bool urunSatistaMi = false;
+
+        ViewData["urunBaslik"] = urunBaslik;
+        ViewData["urunAciklama"] = urunAciklama;
+        ViewData["urunFiyat"] = urunFiyat;
+        ViewData["urunResim"] = urunResim;
+        ViewData["urunSatistaMi"] = urunSatistaMi;
+
         return View();
     }
 }
--- dotnet-basics/obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
+++ dotnet-basics/obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be1c0f74e885ea125a024f0e34fc96a8d3796e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea8cbcbce9c11e5add44066e0b15b19395e7650")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
