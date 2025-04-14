1ea8cbcbce9c11e5add44066e0b15b19395e7650
Soucius <soucius.ggwp@gmail.com>
2025-04-14 11:34:09 +0200

    variables

--- dotnet-basics/Controllers/HomeController.cs
+++ dotnet-basics/Controllers/HomeController.cs
@@ -4,6 +4,17 @@ namespace dotnet_basics.Controllers;
 
 public class HomeController: Controller {
     public ActionResult Index() {
+        int sayi1 = 10;
+        int sayi2 = 20;
+        // int urunFiyat = 5000;
+
+        sayi1 = 30;
+        sayi2 = 40;
+
+        int toplam = sayi1 + sayi2;
+
+        ViewData["Toplam"] = toplam;
+
         return View();
     }
 
--- dotnet-basics/obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
+++ dotnet-basics/obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe81726e268d4ce4d866ded277940c96defe4fab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be1c0f74e885ea125a024f0e34fc96a8d3796e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
