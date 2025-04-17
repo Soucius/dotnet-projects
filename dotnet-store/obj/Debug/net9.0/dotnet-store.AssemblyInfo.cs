c8db461e6525ca4c71a32dea7e8601b1490c5fc1
Soucius <soucius.ggwp@gmail.com>
2025-04-17 14:28:07 +0200

    migrations

--- dotnet-store/obj/Debug/net9.0/dotnet-store.AssemblyInfo.cs
+++ dotnet-store/obj/Debug/net9.0/dotnet-store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b05f972b933bcaaaad2ba4dc1b4241bc539e029")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32d9eb53610d1374aad14d5cb3e2bc395264da9")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
