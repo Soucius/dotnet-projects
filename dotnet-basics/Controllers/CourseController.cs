b5587518c35082fe753f30e75b064f954f48eaf6
Soucius <soucius.ggwp@gmail.com>
2025-04-17 09:47:40 +0200

    dynamic partial views

--- dotnet-basics/Controllers/CourseController.cs
+++ dotnet-basics/Controllers/CourseController.cs
@@ -3,8 +3,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_basics.Controllers;
 public class CourseController: Controller {
+    List<Course> kurslar = new List<Course> {
+        new Course {
+            Title = "Javascript Kursu",
+            Image = "1.jpg",
+            IsActive = true,
+            IsHome = true
+        },
+
+        new Course {
+            Title = "React Kursu",
+            Image = "2.jpg",
+            IsActive = true,
+            IsHome = true
+        },
+
+        new Course {
+            Title= "Angular Kursu",
+            Image= "3.jpg",
+            IsActive = true,
+            IsHome = false
+        },
+
+        new Course {
+            Title= "Nodejs Kursu",
+            Image= "4.jpg",
+            IsActive = true,
+            IsHome = false
+        }
+    };
+    
     public ActionResult Index() {
-        return View();
+        return View(kurslar);
     }
 
     public ActionResult Details() {
@@ -17,32 +47,6 @@ public class CourseController: Controller {
     }
 
     public ActionResult List() {
-        List<Course> kurslar = new List<Course> {
-            new Course {
-                Title = "Javascript Kursu",
-                Image = "1.jpg",
-                IsActive = true
-            },
-
-            new Course {
-                Title = "React Kursu",
-                Image = "2.jpg",
-                IsActive = true
-            },
-
-            new Course {
-                Title= "Angular Kursu",
-                Image= "3.jpg",
-                IsActive = false
-            },
-
-            new Course {
-                Title= "Nodejs Kursu",
-                Image= "4.jpg",
-                IsActive = true
-            }
-        };
-
         return View(kurslar);
     }
 }
--- dotnet-basics/Models/Course.cs
+++ dotnet-basics/Models/Course.cs
@@ -2,8 +2,7 @@ namespace dotnet_basics.Models;
 
 public class Course {
     public string Title { get; set; } = null!;
-
     public string? Image { get; set; }
-
     public bool IsActive { get; set; }
+    public bool IsHome { get; set; }
 }
