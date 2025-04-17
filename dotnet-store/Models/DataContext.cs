a32d9eb53610d1374aad14d5cb3e2bc395264da9
Soucius <soucius.ggwp@gmail.com>
2025-04-17 14:14:54 +0200

    connection string

--- dotnet-store/Models/DataContext.cs
+++ dotnet-store/Models/DataContext.cs
@@ -2,5 +2,9 @@ using Microsoft.EntityFrameworkCore;
 namespace dotnet_store.Models;
 
 public class DataContext : DbContext {
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {
+        
+    }
+
     public DbSet<Urun> Urunler { get; set; }
 }
