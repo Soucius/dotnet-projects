cbb50507c2f347d4b6fe5ab40a4eced531639c38
Soucius <soucius.ggwp@gmail.com>
2025-05-01 09:18:55 +0200

    category entity

--- dotnet-store/Models/DataContext.cs
+++ dotnet-store/Models/DataContext.cs
@@ -12,23 +12,33 @@ public class DataContext : DbContext {
     {
         base.OnModelCreating(modelBuilder);
 
+        modelBuilder.Entity<Kategori>().HasData(
+            new List<Kategori> {
+                new Kategori { Id = 1, KategoriAdi = "Telefon", Url = "telefon" },
+                new Kategori { Id = 2, KategoriAdi = "Beyaz Esya", Url = "beyaz-esya" },
+                new Kategori { Id = 3, KategoriAdi = "Giyim", Url = "giyim" },
+                new Kategori { Id = 4, KategoriAdi = "Kozmetik", Url = "kozmetik" },
+                new Kategori { Id = 5, KategoriAdi = "Elektronik", Url = "elektronik" }
+            }
+        );
+
         modelBuilder.Entity<Urun>().HasData(
             new List<Urun>() {
-                new Urun() { Id = 1, UrunAdi = "Apple Watch 7", Fiyat = 10000, Aktif = false, Resim = "1.jpeg", Anasayfa = true, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi." },
+                new Urun() { Id = 1, UrunAdi = "Apple Watch 7", Fiyat = 10000, Aktif = false, Resim = "1.jpeg", Anasayfa = true, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi.", KategoriId = 1 },
 
-                new Urun() { Id = 2, UrunAdi = "Apple Watch 8", Fiyat = 20000, Aktif = true, Resim = "2.jpeg", Anasayfa = true, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi." },
+                new Urun() { Id = 2, UrunAdi = "Apple Watch 8", Fiyat = 20000, Aktif = true, Resim = "2.jpeg", Anasayfa = true, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi.", KategoriId = 1 },
 
-                new Urun() { Id = 3, UrunAdi = "Apple Watch 9", Fiyat = 30000, Aktif = true, Resim = "3.jpeg", Anasayfa = true, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi." },
+                new Urun() { Id = 3, UrunAdi = "Apple Watch 9", Fiyat = 30000, Aktif = true, Resim = "3.jpeg", Anasayfa = true, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi.", KategoriId = 2 },
 
-                new Urun() { Id = 4, UrunAdi = "Apple Watch 10", Fiyat = 40000, Aktif = false, Resim = "4.jpeg", Anasayfa = false, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi." },
+                new Urun() { Id = 4, UrunAdi = "Apple Watch 10", Fiyat = 40000, Aktif = false, Resim = "4.jpeg", Anasayfa = false, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi.", KategoriId = 2 },
 
-                new Urun() { Id = 5, UrunAdi = "Apple Watch 11", Fiyat = 50000, Aktif = true, Resim = "5.jpeg", Anasayfa = true, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi." },
+                new Urun() { Id = 5, UrunAdi = "Apple Watch 11", Fiyat = 50000, Aktif = true, Resim = "5.jpeg", Anasayfa = true, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi.", KategoriId = 2 },
 
-                new Urun() { Id = 6, UrunAdi = "Apple Watch 12", Fiyat = 60000, Aktif = false, Resim = "6.jpeg", Anasayfa = false, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi." },
+                new Urun() { Id = 6, UrunAdi = "Apple Watch 12", Fiyat = 60000, Aktif = false, Resim = "6.jpeg", Anasayfa = false, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi.", KategoriId = 3 },
 
-                new Urun() { Id = 7, UrunAdi = "Apple Watch 13", Fiyat = 70000, Aktif = false, Resim = "7.jpeg", Anasayfa = false, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi." },
+                new Urun() { Id = 7, UrunAdi = "Apple Watch 13", Fiyat = 70000, Aktif = false, Resim = "7.jpeg", Anasayfa = false, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi.", KategoriId = 3 },
 
-                new Urun() { Id = 8, UrunAdi = "Apple Watch 14", Fiyat = 80000, Aktif = true, Resim = "8.jpeg", Anasayfa = true, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi." }
+                new Urun() { Id = 8, UrunAdi = "Apple Watch 14", Fiyat = 80000, Aktif = true, Resim = "8.jpeg", Anasayfa = true, Aciklama = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti quas, tempora delectus doloribus fugiat! Ullam est corporis explicabo saepe laudantium sunt eligendi, voluptate aspernatur dolore impedit inventore, maiores quasi.", KategoriId = 4 }
             }
         );
     }
