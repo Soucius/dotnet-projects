9a6205549bc7a12d557b936e8dba7127465c949e
Soucius <soucius.ggwp@gmail.com>
2025-05-01 14:11:20 +0200

    navbar ViewComponent

--- dotnet-store/obj/Debug/net9.0/dotnet-store.AssemblyInfo.cs
+++ dotnet-store/obj/Debug/net9.0/dotnet-store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbb50507c2f347d4b6fe5ab40a4eced531639c38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e0445f8aa8be7103631bf691ba103e2a9d4e981")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
