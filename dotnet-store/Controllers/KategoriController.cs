1b6bb16caadc9d86eddc9f4d4f9e777854635bf2
Soucius <soucius.ggwp@gmail.com>
2025-05-03 14:11:13 +0200

    category table

---
+++ dotnet-store/Controllers/KategoriController.cs
@@ -0,0 +1,19 @@
+using dotnet_store.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_store.Controllers;
+
+public class KategoriController : Controller {
+    private readonly DataContext _context;
+
+    public KategoriController(DataContext context) {
+        _context = context;
+    }
+
+    public ActionResult Index() {
+        var kategoriler = _context.Kategoriler.Include(i => i.Uruns).ToList();
+
+        return View(kategoriler);
+    }
+}
